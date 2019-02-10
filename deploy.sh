fcd929ff6c8895b2f35825ff3fa96387226a3d20
snowinszu <86755838@qq.com>
2019-02-10 22:35:05 +0800

    deploy脚本

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-tar -czvf dist.tar.gz dist
-echo "=== scp start ==="
-scp dist.tar.gz ubuntu@oms.cdnbye.com:~/
-echo "=== scp end ==="
-ssh ubuntu@oms.cdnbye.com "tar -xzvf dist.tar.gz"
-echo "=== tar -xzvf  end ==="
-ssh ubuntu@oms.cdnbye.com "sudo rm -rf /var/www/html/oms"
-echo "=== rm  end ==="
-ssh ubuntu@oms.cdnbye.com "sudo mv dist /var/www/html/oms"
-echo "=== mv  end ==="
+# abort on errors
+set -e
+
+# build
+npm run build
+
+rm -rf release
+
+cp -r dist release
+
+# navigate into the build output directory
+cd release
+
+# if you are deploying to a custom domain
+echo 'oms.cdnbye.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/cdnbye/OMS.git master
+
+cd -
