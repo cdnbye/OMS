b1a358f8b745fee1dcf44cb22b91131533806190
lisxure <liushuoxu@gmail.com>
2019-04-27 16:12:08 +0800

    right top ad

--- config/adConfig.js
+++ config/adConfig.js
@@ -4,6 +4,6 @@ export default {
   afterShow: 15000,             //多久显示广告 单位：毫秒
   afterHide: 10000,             //显示后多久自动关闭 单位：毫秒
   leftTopAD: 'lookfor.jpg',     //左上角广告的图片名称 广告图片必须放到 src/assets/ads 文件夹中
-  rightTopAD: 'right-ad.jpg',    //右上角广告的图片名称
+  rightTopAD: 'lookfor.jpg',    //右上角广告的图片名称
   botAD: 'botAD.jpg',           //下方广告的图片名称
 }
