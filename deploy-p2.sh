846232492d8f1fbbc86438f7ef319a99e0ed95fe
snowinszu <86755838@qq.com>
2020-04-23 19:15:45 +0800

    github action

--- deploy-p2.sh
+++ deploy-p2.sh
@@ -21,6 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://gitee.com/cdnbye/OMS2.git master
+# git push -f https://gitee.com/cdnbye/OMS2.git master
+git push -f https://github.com/cdnbye/OMS2.git master
 
 cd -
